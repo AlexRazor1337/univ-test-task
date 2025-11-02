eb4ef5e527da1f53ee038865d0e66add8b3d45f6
AlexRazor <alexrazor1337@gmail.com>
2025-11-02 14:58:48 +0200

    Fix migration

--- apps/products/src/db/drizzle.config.ts
+++ apps/products/src/db/drizzle.config.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: './apps/products/.env' });
 
 export default defineConfig({
-  schema: path.resolve(__dirname, '../../../../libs/db/src/schema'),
+  schema: path.resolve(__dirname, '../../../../libs/db/schema'),
   out: path.resolve(__dirname, './migrations'),
   dialect: 'postgresql',
   dbCredentials: {
